9247b9152e52d59ffa5bc4e8b55ccbd77838865f
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-08-24 17:09:42 +0400

    Add DispatchSemaphore and make currentSessionID public in NetworkSessionsManager

--- VCore/Services and Managers/Network/Session/NetworkSessionManager.swift
+++ VCore/Services and Managers/Network/Session/NetworkSessionManager.swift
@@ -13,8 +13,11 @@ import Foundation
 /// Object contains `shared` instance, but can also be initialized for separate incrementation
 public final class NetworkSessionManager {
     // MARK: Properties
+    private let dispatchSemaphore: DispatchSemaphore = .init(value: 1)
     private let atomicInteger: AtomicInteger = .init(initialValue: 0)
-    private var currentSessionID: Int = -1
+    
+    /// Current session ID. Default to `-1` without any prior incrementation.
+    private(set) public var currentSessionID: Int = -1
     
     /// Shared instance of `NetworkSessionManager`
     public static let shared: AtomicInteger = .init()
@@ -27,6 +30,9 @@ public final class NetworkSessionManager {
 extension NetworkSessionManager {
     /// Generates thread-safe, auto-incremented session identifier from `AtomicInteger`
     public var newSessionID: Int {
+        dispatchSemaphore.wait()
+        defer { dispatchSemaphore.signal() }
+        
         currentSessionID = atomicInteger.value
         return currentSessionID
     }
