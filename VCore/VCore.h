12bb3668f163d6790ef8c03da68d59c79aa437a5
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-08-24 14:30:34 +0400

    Setup project

---
+++ VCore/VCore.h
@@ -0,0 +1,14 @@
+//
+//  VCore.h
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 8/24/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VCore.
+FOUNDATION_EXPORT double VCoreVersionNumber;
+
+//! Project version string for VCore.
+FOUNDATION_EXPORT const unsigned char VCoreVersionString[];
