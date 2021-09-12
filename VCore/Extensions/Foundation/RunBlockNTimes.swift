d5821cd43200b460ac126f212f1bde734132d468
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-09-12 23:15:50 +0400

    Add run block n times

---
+++ VCore/Extensions/Foundation/RunBlockNTimes.swift
@@ -0,0 +1,22 @@
+//
+//  RunBlockNTimes.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 9/12/21.
+//
+
+import Foundation
+
+// MARK:- Run Block n Times
+extension Int {
+    /// Runs block by specified times
+    public func times(_ block: () -> Void) {
+        times { _ in block() }
+    }
+    
+    /// Runs block by specified times, and passes index as parameters
+    public func times(_ block: (Int) -> Void) {
+        guard self > 0 else { return }
+        for i in 0..<self { block(i) }
+    }
+}
