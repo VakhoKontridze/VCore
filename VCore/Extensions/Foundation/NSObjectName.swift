1e943e0fd6e64905b35c85e008a0e6b682ee3952
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-09-12 23:00:31 +0400

    Add NSObject class name

---
+++ VCore/Extensions/Foundation/NSObjectName.swift
@@ -0,0 +1,21 @@
+//
+//  NSObjectName.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 9/12/21.
+//
+
+import Foundation
+
+// MARK:- NS Object Name
+extension NSObject {
+    /// `NSObject` class name full
+    public static var nsObjectClassNameFull: String {
+        NSStringFromClass(self)
+    }
+    
+    /// `NSObject` class name
+    public static var nsObjectClassName: String? {
+        nsObjectClassNameFull.components(separatedBy: ".").last
+    }
+}
