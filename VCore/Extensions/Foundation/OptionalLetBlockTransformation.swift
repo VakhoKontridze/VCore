3dc7792e6f6b6d22bc2917ab7f1e834256b0b5ff
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-09-12 23:03:50 +0400

    Add optional let block transformation

---
+++ VCore/Extensions/Foundation/OptionalLetBlockTransformation.swift
@@ -0,0 +1,23 @@
+//
+//  OptionalLetBlockTransformation.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 9/12/21.
+//
+
+import Foundation
+
+// MARK:- Optional Let Block Transformation
+extension Optional where Wrapped: Any {
+    /// Transforms wrapped value of optional, or returns `nil`
+    public func `let`<T>(_ transform: (Wrapped) throws -> T?) rethrows -> T? {
+        guard
+            let self = self,
+            let result = try transform(self)
+        else {
+            return nil
+        }
+        
+        return result
+    }
+}
