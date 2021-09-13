467acb90ebc6d0bf589d5173ecf521bf8a80cbad
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-09-13 20:43:32 +0400

    Add key path sorting

---
+++ VCore/Extensions/Foundation/KeyPathSort.swift
@@ -0,0 +1,18 @@
+//
+//  KeyPathSort.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 9/13/21.
+//
+
+import Foundation
+
+// MARK:- KeyPath Sort
+extension Sequence {
+    /// Sorts sequence by a key path
+    public func sorted<T: Comparable>(
+        by keypath: KeyPath<Element, T>
+    ) -> [Element] {
+        sorted { $0[keyPath: keypath] < $1[keyPath: keypath] }
+    }
+}
