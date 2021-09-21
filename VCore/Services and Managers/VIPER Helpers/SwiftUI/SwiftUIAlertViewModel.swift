de2832d51501591e21518bd3ba49925295157ccb
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-09-21 11:40:20 +0400

    Add SwiftUI Viper helpers

---
+++ VCore/Services and Managers/VIPER Helpers/SwiftUI/SwiftUIAlertViewModel.swift
@@ -0,0 +1,146 @@
+//
+//  SwiftUIAlertViewModel.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 9/21/21.
+//
+
+import SwiftUI
+
+// MARK:- SwiftUI Alert ViewModel
+/// SwiftUI Alert ViewModel
+///
+/// Viewmodel for presenting an `Alert`.
+///
+/// In `VIP` and `VIPER` arhcitecutes, viewmodel is stored in`Presenter`.
+public enum SwiftUIAlertViewModel {
+    /// One button
+    case oneButton(viewModel: OneButtonViewModel)
+    
+    /// Two button
+    case twoButtons(viewModel: TwoButtonsViewModel)
+}
+
+// MARK:- Button
+extension SwiftUIAlertViewModel {
+    /// Alert Button ViewModel
+    public struct AlertButtonViewModel {
+        /// Button title
+        public let title: String
+        
+        /// Button action
+        public let action: (() -> Void)?
+        
+        /// Initializes viewmodel
+        public init(
+            title: String,
+            action: (() -> Void)?
+        ) {
+            self.title = title
+            self.action = action
+        }
+    }
+}
+
+// MARK:- One Button ViewModel
+extension SwiftUIAlertViewModel {
+    /// One Button ViewModel
+    public struct OneButtonViewModel {
+        /// Alert title
+        public let title: String
+        
+        /// Alert message
+        public let message: String?
+        
+        /// Alert dismiss button
+        public let dismissButton: AlertButtonViewModel
+        
+        /// Initializes viewmodel
+        public init(
+            title: String,
+            message: String?,
+            dismissButton: AlertButtonViewModel
+        ) {
+            self.title = title
+            self.message = message
+            self.dismissButton = dismissButton
+        }
+    }
+}
+
+// MARK:- Two Buttons ViewModel
+extension SwiftUIAlertViewModel {
+    /// Two Buttons ViewModel
+    public struct TwoButtonsViewModel {
+        /// Alert title
+        public let title: String
+        
+        /// Alert message
+        public let message: String?
+        
+        /// Alert primary button
+        public let primaryButton: AlertButtonViewModel
+        
+        /// Alert secondary button
+        public let secondaryButton: AlertButtonViewModel
+        
+        /// Initializes viewmodel
+        public init(
+            title: String,
+            message: String?,
+            primaryButton: AlertButtonViewModel,
+            secondaryButton: AlertButtonViewModel
+        ) {
+            self.title = title
+            self.message = message
+            self.primaryButton = primaryButton
+            self.secondaryButton = secondaryButton
+        }
+    }
+}
+
+// MARK:- Factory
+extension View {
+    /// Presents `Alert` when `viewModel` parameter is non-nil
+    @ViewBuilder public func alert(
+        viewModel: Binding<SwiftUIAlertViewModel?>
+    ) -> some View {
+        let nulingAction: () -> Void = { viewModel.wrappedValue = nil }
+        
+        switch viewModel.wrappedValue {
+        case nil:
+            self
+            
+        case .oneButton(let oneButtonViewModel):
+            self.alert(isPresented: .constant(true), content: {
+                Alert(
+                    title: .init(oneButtonViewModel.title),
+                    message: oneButtonViewModel.message.let { .init($0) },
+                    dismissButton: .build(viewModel: oneButtonViewModel.dismissButton, completion: nulingAction)
+                )
+            })
+            
+        case .twoButtons(let twoButtonsViewModel):
+            self.alert(isPresented: .constant(true), content: {
+                Alert(
+                    title: .init(twoButtonsViewModel.title),
+                    message: twoButtonsViewModel.message.let { .init($0) },
+                    primaryButton: .build(viewModel: twoButtonsViewModel.primaryButton, completion: nulingAction),
+                    secondaryButton: .build(viewModel: twoButtonsViewModel.secondaryButton, completion: nulingAction)
+                )
+            })
+        }
+    }
+}
+
+extension Alert.Button {
+    fileprivate static func build(
+        viewModel: SwiftUIAlertViewModel.AlertButtonViewModel,
+        completion: @escaping () -> Void
+    ) -> Alert.Button {
+        .default(.init(viewModel.title), action: {
+            viewModel.action?()
+            completion()
+        })
+    }
+}
---
+++ VCore/Services and Managers/VIPER Helpers/SwiftUI/SwiftUIProgressViewViewModel.swift
@@ -0,0 +1,75 @@
+//
+//  SwiftUIProgressViewViewModel.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 9/21/21.
+//
+
+import SwiftUI
+
+// MARK:- SwiftUI Progress View ViewModel
+/// SwiftUI Progress View ViewModel
+///
+/// Viewmodel for presenting an `ProgressView`.
+///
+/// In `VIP` and `VIPER` arhcitecutes, viewmodel is stored in `Presenter`.
+@available(iOS 14, *)
+public struct SwiftUIProgressViewViewModel {
+    // MARK: Properties
+    public let isInteractionDisabled: Bool
+    public let scalingFactor: CGFloat?
+    public let color: Color?
+    
+    // MARK: Initializers
+    public init(
+        isInteractionDisabled: Bool,
+        scalingFactor: CGFloat? = nil,
+        color: Color? = nil
+    ) {
+        self.isInteractionDisabled = isInteractionDisabled
+        self.scalingFactor = scalingFactor
+        self.color = color
+    }
+}
+
+// MARK:- Factory
+extension View {
+    /// Presents `ProgressView` when `viewModel` parameter is non-nil
+    @available(iOS 14, *)
+    @ViewBuilder public func progressView(
+        viewModel: SwiftUIProgressViewViewModel?
+    ) -> some View {
+        switch viewModel {
+        case nil:
+            self
+            
+        case let viewModel?:
+            self
+                .disabled(viewModel.isInteractionDisabled)
+                .overlay(
+                    ProgressView()
+                        .scaleEffect(viewModel: viewModel)
+                        .progressViewStyle(viewModel: viewModel)
+                )
+        }
+    }
+}
+
+// MARK:- Factory
+extension View {
+    @available(iOS 14, *)
+    @ViewBuilder fileprivate func progressViewStyle(viewModel: SwiftUIProgressViewViewModel) -> some View {
+        switch viewModel.color {
+        case nil: self.progressViewStyle(CircularProgressViewStyle())
+        case let color?: self.progressViewStyle(CircularProgressViewStyle(tint: color))
+        }
+    }
+    
+    @available(iOS 14, *)
+    @ViewBuilder fileprivate func scaleEffect(viewModel: SwiftUIProgressViewViewModel) -> some View {
+        switch viewModel.scalingFactor {
+        case nil: self
+        case let scalingFactor?: self.scaleEffect(x: scalingFactor, y: scalingFactor, anchor: .center)
+        }
+    }
+}
