0d4d50d2a923cce67eb64df9b3474118544e46a6
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-12-29 19:58:53 +0400

    Add KeyPath sort method to array

--- VCore/Extensions/Foundation/Sequence.KeyPathSort.swift
+++ VCore/Extensions/Foundation/Sequence.KeyPathSort.swift
@@ -9,10 +9,19 @@ import Foundation
 
 // MARK: - KeyPath Sort
 extension Sequence {
-    /// Returns the elements of the sequence, sorted using the given keypath as the comparison between elements.
+    /// Returns the elements of a sequence, sorted using the given keypath as the comparison between elements.
     public func sorted<T: Comparable>(
         by keypath: KeyPath<Element, T>
     ) -> [Element] {
         sorted { $0[keyPath: keypath] < $1[keyPath: keypath] }
+    }
+}
+
+extension Array {
+    /// Sorts the elements of an array using the given keypath as the comparison between elements.
+    public mutating func sort<T: Comparable>(
+        by keypath: KeyPath<Element, T>
+    ) {
+        sort { $0[keyPath: keypath] < $1[keyPath: keypath] }
     }
 }
