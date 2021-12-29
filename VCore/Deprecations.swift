6754401e4d0bb1d7374af2944490a797b3ba5e6f
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2021-12-29 20:27:36 +0400

    Rename NetworkProcessor to NetworkResponseProcessor

--- VCore/Deprecations.swift
+++ VCore/Deprecations.swift
@@ -6,3 +6,14 @@
 //
 
 import UIKit
+
+// MARK: - Network Response Processor
+@available(*, deprecated, renamed: "NetworkResponseProcessor")
+public typealias NetworkProcessor = NetworkResponseProcessor
+
+extension NetworkClient {
+    @available(*, deprecated, message: "Use init with `responseProcessor` parameter")
+    public convenience init(processor: NetworkResponseProcessor) {
+        self.init(responseProcessor: processor)
+    }
+}
