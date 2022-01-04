186cdc6fe0a47bfb12d48bbf8e5ca1d2863f47ce
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-01-04 13:39:13 +0400

    Reorder parameters in UILabel configuration methods

--- VCore/Extensions/UIKit/UILabel.Configuration.swift
+++ VCore/Extensions/UIKit/UILabel.Configuration.swift
@@ -9,22 +9,22 @@ import UIKit
 
 // MARK: - Label Configuration
 extension UILabel {
-    /// Configures `UILabel` with alignment, number of lines, line break mode, color, and font.
+    /// Configures `UILabel` with alignment, line break mode, number of lines, color, and font.
     public func configure(
         alignment: NSTextAlignment = .natural,
-        numberOfLines: Int = 1,
         lineBreakMode: NSLineBreakMode = .byTruncatingTail,
+        numberOfLines: Int = 1,
         color: UIColor?,
         font: UIFont?
     ) {
         self.textAlignment = alignment
-        self.numberOfLines = numberOfLines
         self.lineBreakMode = lineBreakMode
+        self.numberOfLines = numberOfLines
         self.textColor = color
         self.font = font
     }
     
-    /// Initializes `UILabel` with alignment, number of lines, line break mode, color, and font.
+    /// Initializes `UILabel` with alignment, line break mode, number of lines, color, and font.
     public convenience init(
         alignment: NSTextAlignment = .natural,
         lineBreakMode: NSLineBreakMode = .byTruncatingTail,
@@ -36,8 +36,8 @@ extension UILabel {
         
         configure(
             alignment: alignment,
-            numberOfLines: numberOfLines,
             lineBreakMode: lineBreakMode,
+            numberOfLines: numberOfLines,
             color: color,
             font: font
         )
