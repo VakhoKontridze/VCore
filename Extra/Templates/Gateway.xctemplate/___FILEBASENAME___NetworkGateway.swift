1d2201e3a32e164bb0b8d199934bbd23e1ecd48b
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-02-14 12:32:48 +0400

    Add with parameter name to Gateway in XCode Templates

--- Extra/Templates/Gateway.xctemplate/___FILEBASENAME___NetworkGateway.swift
+++ Extra/Templates/Gateway.xctemplate/___FILEBASENAME___NetworkGateway.swift
@@ -4,7 +4,7 @@ import Foundation
 
 // MARK: - ___VARIABLE_productName___ Network Gateway
 struct ___VARIABLE_productName___NetworkGateway: ___VARIABLE_productName___Gatewayable {
-    func fetch(parameters: ___VARIABLE_productName___Parameters) async throws -> ___VARIABLE_productName___Entity {
+    func fetch(with parameters: ___VARIABLE_productName___Parameters) async throws -> ___VARIABLE_productName___Entity {
         fatalError()
     }
 }
