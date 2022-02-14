34121703b722afc6f74a8cccd3ad025d7ef61251
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-02-14 12:35:02 +0400

    Prepare for 2.2.1

--- Package.swift
+++ Package.swift
@@ -19,8 +19,8 @@ let package: Package = .init(
     targets: [
         .binaryTarget(
             name: "VCore",
-            url: "https://github.com/VakhoKontridze/VCore/releases/download/2.2.0/VCore.xcframework.zip",
-            checksum: "a50a48b69c11e658cc8a197f7ee557f98a5153216928d294fc41c118c76971e2"
+            url: "https://github.com/VakhoKontridze/VCore/releases/download/2.2.1/VCore.xcframework.zip",
+            checksum: "d8ddd4b4d0497b1dcf999a1a273cf905536e59b0164a3e26874fdd851bf72242"
         ),
     ]
 )
