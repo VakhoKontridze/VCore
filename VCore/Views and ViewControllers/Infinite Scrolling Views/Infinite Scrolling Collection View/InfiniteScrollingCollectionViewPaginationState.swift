cfaa5b51b37ab64d626f81cf9a07ca21382eb52a
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-02-25 17:55:23 +0400

    Add ScrollableView

--- VCore/Views and ViewControllers/Infinite Scrolling Views/Infinite Scrolling Collection View/InfiniteScrollingCollectionViewPaginationState.swift
+++ VCore/Views and ViewControllers/Infinite Scrolling Views/Infinite Scrolling Collection View/InfiniteScrollingCollectionViewPaginationState.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-// MARK: - Pagination State
+// MARK: - Infinite Scrolling Collection View Pagination State
 extension InfiniteScrollingCollectionView {
     public typealias PaginationState = InfiniteScrollingPaginationState
 }
---
+++ VCore/Views and ViewControllers/Scrollable View/ScrollableView.ScrollDirection.swift
@@ -0,0 +1,31 @@
+//
+//  ScrollableView.ScrollDirection.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 2/25/22.
+//
+
+import Foundation
+
+// MARK: - Scrollable View Scroll Direction
+extension ScrollableView {
+    /// Scroll Direction.
+    public struct ScrollDirection: OptionSet {
+        // MARK: Properties
+        public let rawValue: Int
+
+        /// Horizontal scroll direction.
+        public static var horizontal: ScrollDirection { .init(rawValue: 1 << 0) }
+        
+        /// Vertical scroll direction.
+        public static var vertical: ScrollDirection { .init(rawValue: 1 << 1) }
+        
+        /// Horizontal and vertical scroll directions.
+        public static var all: ScrollDirection { [.horizontal, .vertical] }
+        
+        // MARK: Initializers
+        public init(rawValue: Int) {
+            self.rawValue = rawValue
+        }
+    }
+}
