c2679c497f4ade58b3f6e4ed7b78bc6d4f2a50ce
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-04-28 22:01:49 +0400

    Add JSONRequestHeaders

---
+++ Sources/VCore/Services and Managers/Network Client/Headers/JSONAuthorizedRequestHeaders.swift
@@ -0,0 +1,45 @@
+//
+//  JSONAuthorizedRequestHeaders.swift
+//  
+//
+//  Created by Vakhtang Kontridze on 28.04.22.
+//
+
+import Foundation
+
+// MARK: - JSON Authorized Request Headers
+/// Authorized JSON request headers that pass `application/json` as `accept`, `application/json` as `contentType`, and `authorization` as `Bearer \(token)`.
+///
+/// Can be used in `NetworkClient`.
+public struct JSONAuthorizedRequestHeaders: Encodable {
+    // MARK: Properties
+    /// Accept. Defaults to `application/json`.
+    public let accept: String = "application/json"
+    
+    /// Content type. Defaults to `application/json`.
+    public let contentType: String = "application/json"
+    
+    /// Authorization
+    public let authorization: String
+    
+    // MARK: Initializers
+    /// Initializes `JSONAuthorizedRequestHeaders` with token.
+    public init(token: String) {
+        self.authorization = "Bearer \(token)"
+    }
+    
+    // MARK: Coding Keys
+    enum CodingKeys: String, CodingKey {
+        case accept = "Accept"
+        case contentType = "Content-Type"
+        case authorization = "Authorization"
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container: KeyedEncodingContainer<CodingKeys> = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(accept, forKey: .accept)
+        try container.encode(contentType, forKey: .contentType)
+        try container.encode(authorization, forKey: .authorization)
+    }
+}
---
+++ Sources/VCore/Services and Managers/Network Client/Headers/MultiPartFormDataAuthorizedRequestHeaders.swift
@@ -0,0 +1,46 @@
+//
+//  MultiPartFormDataAuthorizedRequestHeaders.swift
+//  
+//
+//  Created by Vakhtang Kontridze on 28.04.22.
+//
+
+import Foundation
+
+// MARK: - Multi Part Form Data Authorized Request Headers
+/// JSON request headers that pass `application/json` as `accept`, `multipart/form-data; boundary=\(boundary)` as `contentType`, and `authorization` as `Bearer \(token)`.
+///
+/// Can be used in `NetworkClient` with `MultiPartFormDataBuilder`.
+struct MultiPartFormDataAuthorizedRequestHeaders: Encodable {
+    // MARK: Properties
+    /// Accept. Defaults to `application/json`.
+    public let accept: String = "application/json"
+    
+    /// Content type.
+    public let contentType: String
+    
+    /// Authorization.
+    public let authorization: String
+    
+    // MARK: Initializers
+    /// Initializes `MultiPartFormDataRequestHeaders` with boundary nad token.
+    public init(boundary: String, token: String) {
+        self.contentType = "multipart/form-data; boundary=\(boundary)"
+        self.authorization = "Bearer \(token)"
+    }
+    
+    // MARK: Coding Keys
+    enum CodingKeys: String, CodingKey {
+        case accept = "Accept"
+        case contentType = "Content-Type"
+        case authorization = "Authorization"
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container: KeyedEncodingContainer<CodingKeys> = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(accept, forKey: .accept)
+        try container.encode(contentType, forKey: .contentType)
+        try container.encode(authorization, forKey: .authorization)
+    }
+}
