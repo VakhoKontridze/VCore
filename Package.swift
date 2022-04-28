878810fd0c05667de8cdd1178584b2a6b73c4638
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-04-28 13:17:07 +0400

    Track missing files

--- Package.swift
+++ Package.swift
@@ -17,10 +17,13 @@ let package: Package = .init(
     ],
     
     targets: [
-        .binaryTarget(
+        .target(
             name: "VCore",
-            url: "https://github.com/VakhoKontridze/VCore/releases/download/2.3.0/VCore.xcframework.zip",
-            checksum: "a5cef415d0b7db4fc6cba88bf190f95433058a805770a3c3fec07b260b3510c9"
+            dependencies: []
+        ),
+        .testTarget(
+            name: "VCoreTests",
+            dependencies: ["VCore"]
         ),
     ]
 )
