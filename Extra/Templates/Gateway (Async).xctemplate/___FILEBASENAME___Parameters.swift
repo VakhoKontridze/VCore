552df2b3c0629ba32fcd600b985ea140b354c5dd
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-05-01 23:58:45 +0400

    Add completion-based Gateway to XCode templates

---
+++ Extra/Templates/Gateway (Async).xctemplate/___FILEBASENAME___Parameters.swift
@@ -0,0 +1,13 @@
+//  ___FILEHEADER___
+
+import Foundation
+
+// MARK: - ___VARIABLE_productName___ Parameters
+struct ___VARIABLE_productName___Parameters: Encodable {
+    // MARK: Properties
+    
+    // MARK: Coding Keys
+    enum CodingKeys: String, CodingKey {
+        
+    }
+}
