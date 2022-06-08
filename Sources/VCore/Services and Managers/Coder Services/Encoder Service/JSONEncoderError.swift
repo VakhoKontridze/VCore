d2994380c9b831f96651fb6156f500f086fb40ff
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-06-08 10:30:25 +0400

    Remove plus one increment from error codes of all VCoreError types

--- Sources/VCore/Services and Managers/Coder Services/Encoder Service/JSONEncoderError.swift
+++ Sources/VCore/Services and Managers/Coder Services/Encoder Service/JSONEncoderError.swift
@@ -14,14 +14,8 @@ public enum JSONEncoderError: Int, VCoreError, CaseIterable {
     /// Indicates that data cannot be encoded.
     case failedToEncode
     
-    // MARK: Properties
-    // Overriden
-    public static var errorDomain: String { "com.vcore.jsonencoderservice" }
-    
     // MARK: VCore Error
-    public var code: Int { 1000 + (rawValue+1) }
-    
-    public var description: String {
-        VCoreLocalizationService.shared.localizationProvider.value(jsonEncoderError: self)
-    }
+    public static var errorDomain: String { "com.vcore.jsonencoderservice" }
+    public var code: Int { 1000 + rawValue }
+    public var description: String { VCoreLocalizationService.shared.localizationProvider.value(jsonEncoderError: self) }
 }
