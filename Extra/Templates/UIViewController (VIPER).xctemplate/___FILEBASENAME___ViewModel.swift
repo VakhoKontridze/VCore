b876b9aadaf428d2608aa39cfca40da0ea77a948
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-06-08 13:24:37 +0400

    Rename UIKit VIPER scene templates folders

--- Extra/Templates/UIViewController (VIPER).xctemplate/___FILEBASENAME___ViewModel.swift
+++ Extra/Templates/UIViewController (VIPER).xctemplate/___FILEBASENAME___ViewModel.swift
@@ -4,5 +4,8 @@ import Foundation
 
 // MARK: - ___VARIABLE_productName___ ViewModel
 struct ___VARIABLE_productName___ViewModel {
+    // MARK: Properties
     
+    // MARK: Preview
+    static var mock: Self { .init() }
 }
