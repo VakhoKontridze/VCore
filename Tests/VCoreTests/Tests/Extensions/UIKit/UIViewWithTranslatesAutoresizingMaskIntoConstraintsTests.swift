4b4ae8f3b64b0f3823b6fed286937f9238907ff3
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-06-10 11:42:16 +0400

    Add withTranslatesAutoresizingMaskIntoConstraints method to UIView

---
+++ Tests/VCoreTests/Tests/Extensions/UIKit/UIViewWithTranslatesAutoresizingMaskIntoConstraintsTests.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewWithTranslatesAutoresizingMaskIntoConstraintsTests.swift
+//  VCoreTests
+//
+//  Created by Vakhtang Kontridze on 10.06.22.
+//
+
+#if canImport(UIKit) && !os(watchOS)
+
+import XCTest
+@testable import VCore
+
+// MARK: - Tests
+final class UIViewWithTranslatesAutoresizingMaskIntoConstraintsTests: XCTestCase {
+    func test() {
+        let view1: UILabel = .init().withTranslatesAutoresizingMaskIntoConstraints(false)
+        XCTAssertEqual(view1.translatesAutoresizingMaskIntoConstraints, false)
+        
+        let view2: UILabel = .init().withTranslatesAutoresizingMaskIntoConstraints(true)
+        XCTAssertEqual(view2.translatesAutoresizingMaskIntoConstraints, true)
+    }
+}
+
+#endif
