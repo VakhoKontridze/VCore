85c58cc48219b62ed155f2c22934e0edbfed1d84
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-06-13 13:05:31 +0400

    Move CACornerMask and UIRectCorner extensions to separate files, and write tests

--- Sources/VCore/Extensions/UIKit/UIView.RoundCorners.swift
+++ Sources/VCore/Extensions/UIKit/UIView.RoundCorners.swift
@@ -29,32 +29,4 @@ extension UIView {
     }
 }
 
-// MARK: - CA Corner Mask Option Set
-extension CACornerMask {
-    /// Top corners.
-    ///
-    /// Includes `layerMinXMinYCorner` and  `layerMaxXMinYCorner`.
-    public static var layerMinYCorners: CACornerMask { [.layerMinXMinYCorner, .layerMaxXMinYCorner] }
-    
-    /// Right corners.
-    ///
-    /// Includes `layerMaxXMinYCorner` and  `layerMaxXMaxYCorner`.
-    public static var layerMaxXCorners: CACornerMask { [.layerMaxXMinYCorner, .layerMaxXMaxYCorner] }
-    
-    /// Bottom corners.
-    ///
-    /// Includes `layerMinXMaxYCorner` and  `layerMaxXMaxYCorner`.
-    public static var layerMaxYCorners: CACornerMask { [.layerMinXMaxYCorner, .layerMaxXMaxYCorner] }
-    
-    /// Left corners.
-    ///
-    /// Includes `layerMinXMinYCorner` and  `layerMinXMaxYCorner`.
-    public static var layerMinXCorners: CACornerMask { [.layerMinXMinYCorner, .layerMinXMaxYCorner] }
-    
-    /// All corners.
-    ///
-    /// Includes `layerMinXMinYCorner`, `layerMaxXMinYCorner`, `layerMaxXMaxYCorner`, and  `layerMinXMaxYCorner`.
-    public static var layerAllCorners: CACornerMask { [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner, .layerMinXMaxYCorner] }
-}
-
 #endif
---
+++ Tests/VCoreTests/Tests/Extensions/UIKit/UIRectCornerAdditionalOptionsTests.swift
@@ -0,0 +1,24 @@
+//
+//  UIRectCornerAdditionalOptionsTests.swift
+//  VCoreTests
+//
+//  Created by Vakhtang Kontridze on 13.06.22.
+//
+
+#if canImport(UIKit)
+
+import XCTest
+@testable import VCore
+
+// MARK: - Tests
+final class UIRectCornerAdditionalOptionsTests: XCTestCase {
+    func test() {
+        // `rawValue` of `allCorners` is `18446744073709551615`
+        let allCornersRawValue: UInt = UIRectCorner([.topLeft, .topRight, .bottomRight, .bottomLeft]).rawValue
+        
+        XCTAssertEqual(UIRectCorner.leftCorners.union(.rightCorners).rawValue, allCornersRawValue)
+        XCTAssertEqual(UIRectCorner.topCorners.union(.bottomCorners).rawValue, allCornersRawValue)
+    }
+}
+
+#endif
