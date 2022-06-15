486e42bf799a467317fc31a35b7958767d437214
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-06-15 18:54:50 +0400

    Fix compilation error in InfiniteScrollingTableView

--- Sources/VCore/Views and ViewControllers/Infinite Scrolling Views/Infinite Scrolling Table View/InfiniteScrollingTableView.swift
+++ Sources/VCore/Views and ViewControllers/Infinite Scrolling Views/Infinite Scrolling Table View/InfiniteScrollingTableView.swift
@@ -24,7 +24,7 @@ import UIKit
 open class InfiniteScrollingTableView: UITableView {
     // MARK: Properties
     /// Delegate.
-    open weak var infiniteScrollingDelegate: (any InfiniteScrollingTableViewDelegate & UITableViewDataSource & any UIScrollViewDelegate)?
+    open weak var infiniteScrollingDelegate: (any InfiniteScrollingTableViewDelegate & UITableViewDataSource & UIScrollViewDelegate)?
     
     /// Controls pagination state.
     /// When insufficient data is loaded in`UITableView`, or when pagination occurs, property is set to `.loading` and delegate method is called.
