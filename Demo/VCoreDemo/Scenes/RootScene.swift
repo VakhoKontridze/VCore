26fdadae2fa191b883e8dcfe66ac260a16961326
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-06-17 15:40:38 +0400

    Change ViewController background color

--- Demo/VCoreDemo/Scenes/RootScene.swift
+++ Demo/VCoreDemo/Scenes/RootScene.swift
@@ -14,7 +14,7 @@ import VCore
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
     }
 }
 
