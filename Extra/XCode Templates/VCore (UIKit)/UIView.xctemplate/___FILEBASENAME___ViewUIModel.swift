6a44642db41d1f70359058a6908cd732d6371a38
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-07-11 12:39:30 +0400

    Remove optionals from UIColors in XCode templates

--- Extra/XCode Templates/VCore (UIKit)/UIView.xctemplate/___FILEBASENAME___ViewUIModel.swift
+++ Extra/XCode Templates/VCore (UIKit)/UIView.xctemplate/___FILEBASENAME___ViewUIModel.swift
@@ -18,7 +18,7 @@ struct ___VARIABLE_productName___ViewUIModel {
     // MARK: Colors
     struct Colors {
         // MARK: Properties
-        static var background: UIColor? { .systemBackground }
+        static var background: UIColor { .systemBackground }
         
         // MARK: Initializers
         private init() {}
