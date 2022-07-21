38a03a5d3b93f250c4e15e322312ec0868955c20
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-07-21 23:32:50 +0400

    Enable RawRepresentable KeychainStorage

--- Sources/VCore/Models/Property Wrappers/Keychain Storage/KeychainStorage.swift
+++ Sources/VCore/Models/Property Wrappers/Keychain Storage/KeychainStorage.swift
@@ -134,30 +134,30 @@ extension KeychainStorage where Value: Codable, Value: ExpressibleByNilLiteral {
 }
 
 // MARK: Initializers - Raw Representable
-//extension KeychainStorage where Value: RawRepresentable, Value.RawValue: Codable {
-//    /// Initializes `KeychainStorage` from `RawRepresentable`.
-//    public init(
-//        wrappedValue defaultValue: Value,
-//        _ key: String
-//    ) {
-//        self.init(
-//            initialValue: .init(rawValue: Self.getValue(key: key, defaultValue: defaultValue.rawValue))!,
-//            valueSetter: { Self.setValue($0.rawValue, key: key) }
-//        )
-//    }
-//}
-//
-//extension KeychainStorage where Value: RawRepresentable, Value.RawValue: Codable, Value: ExpressibleByNilLiteral {
-//    /// Initializes `KeychainStorage` from `nil` `RawRepresentable` literal.
-//    public init(
-//        _ key: String
-//    ) {
-//        self.init(
-//            wrappedValue: nil,
-//            key
-//        )
-//    }
-//}
+extension KeychainStorage where Value: RawRepresentable, Value.RawValue: Codable {
+    /// Initializes `KeychainStorage` from `RawRepresentable`.
+    public init(
+        wrappedValue defaultValue: Value,
+        rawRepresented key: String
+    ) {
+        self.init(
+            initialValue: .init(rawValue: Self.getValue(key: key, defaultValue: defaultValue.rawValue))!,
+            valueSetter: { Self.setValue($0.rawValue, key: key) }
+        )
+    }
+}
+
+extension KeychainStorage where Value: RawRepresentable, Value.RawValue: Codable, Value: ExpressibleByNilLiteral {
+    /// Initializes `KeychainStorage` from `nil` `RawRepresentable` literal.
+    public init(
+        rawRepresented key: String
+    ) {
+        self.init(
+            wrappedValue: nil,
+            rawRepresented: key
+        )
+    }
+}
 
 // MARK: - Observable Storage
 private final class ObservableStorage<Value>: ObservableObject {
