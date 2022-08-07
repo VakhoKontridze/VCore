4b3e3af150e28b8246f87a7fb8430bac8399b492
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-08-07 22:14:46 +0400

    Add documentation to NavigationStackCoordinable

--- Sources/VCore/Helpers/Architectural Pattern Helpers/SwiftUI/Coordinating Navigation Stack/NavigationStackCoordinable.swift
+++ Sources/VCore/Helpers/Architectural Pattern Helpers/SwiftUI/Coordinating Navigation Stack/NavigationStackCoordinable.swift
@@ -9,6 +9,10 @@ import SwiftUI
 
 // MARK: - Navigation Stack Coordinable
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
+/// Protocol for managing `NavigationPath` within `NavigationStackCoordinator`.
+///
+/// In `MVP`, `VIP`, and `VIPER` architectures, this protocol is conformed to by a `Presenter`.
+/// in `MVVM` architecture, this protocol is conformed to by a `ViewModel.`
 @MainActor public protocol NavigationStackCoordinable: ObservableObject {
     /*@Published*/ var navigationStackCoordinator: NavigationStackCoordinator? { get set }
 }
