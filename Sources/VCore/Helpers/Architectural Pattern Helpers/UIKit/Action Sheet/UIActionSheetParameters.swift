0c16fba4303564587ffb9cd6ea1a97a820000159
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2022-08-10 14:42:01 +0400

    Move UIActionSheetParameters to separate file

---
+++ Sources/VCore/Helpers/Architectural Pattern Helpers/UIKit/Action Sheet/UIActionSheetParameters.swift
@@ -0,0 +1,57 @@
+//
+//  UIActionSheetParameters.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 10.08.22.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+// MARK: - UI Action Sheet Parameters
+/// Parameters for presenting an `UIActionSheet`.
+///
+/// In `MVP`, `VIP`, and `VIPER` architectures, parameters are passed by`Presenter` to `View/Controller`
+/// In `MVVM` architecture, parameters are passed by`ViewModel` to `View/Controller`
+///
+/// For usage example, refer to `UIActionSheetViewable`.
+public struct UIActionSheetParameters {
+    // MARK: Properties
+    /// Action sheet title.
+    public var title: String?
+    
+    /// Action sheet message.
+    public var message: String?
+    
+    /// Buttons.
+    public var buttons: () -> [any UIActionSheetButtonProtocol]
+    
+    // MARK: Initializers
+    /// Initializes `UIActionSheetParameters`.
+    public init(
+        title: String?,
+        message: String?,
+        @UIActionSheetButtonBuilder actions buttons: @escaping () -> [any UIActionSheetButtonProtocol]
+    ) {
+        self.title = title
+        self.message = message
+        self.buttons = buttons
+    }
+}
+
+// MARK: - Factory
+extension UIAlertController {
+    /// Initializes `UIAlertController` with `UIActionSheetParameters`.
+    public convenience init(parameters: UIActionSheetParameters) {
+        self.init(
+            title: parameters.title,
+            message: parameters.message,
+            preferredStyle: .actionSheet
+        )
+
+        parameters.buttons().forEach { addAction($0.toUIAlertAction) }
+    }
+}
+
+#endif
