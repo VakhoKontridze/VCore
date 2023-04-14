de30aa4926d93f830df7a9305f1620f60a24626a
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2023-04-14 18:11:48 +0400

    Move View.customDismissAction(_:) method to CustomDismissActionEnvironmentValue.swift

--- Sources/VCore/Models/Custom Dismiss Action/CustomDismissActionEnvironmentValue.swift
+++ Sources/VCore/Models/Custom Dismiss Action/CustomDismissActionEnvironmentValue.swift
@@ -7,6 +7,44 @@
 
 import SwiftUI
 
+// MARK: - Custom Dismiss Action Extension
+extension View {
+    /// Sets the environment value of `CustomDismissAction` to the given value.
+    ///
+    ///     @State private var isPresented: Bool = false
+    ///
+    ///     var body: some View {
+    ///         Button("Present", action: { isPresented = true })
+    ///             .fullScreenCover(
+    ///                 isPresented: $isPresented,
+    ///                 content: {
+    ///                     Destination()
+    ///                         .customDismissAction(CustomDismissAction({
+    ///                             print("Do something here")
+    ///                             isPresented = false
+    ///                         }))
+    ///                 }
+    ///             )
+    ///     }
+    ///
+    ///     struct Destination: View {
+    ///         @Environment(\.customDismissAction) private var customDismissAction: CustomDismissAction
+    ///
+    ///         var body: some View {
+    ///             Color.accentColor
+    ///                 .ignoresSafeArea()
+    ///                 .onTapGesture(perform: { customDismissAction.action() })
+    ///         }
+    ///     }
+    ///
+    public func customDismissAction(
+        _ customDismissAction: CustomDismissAction
+    ) -> some View {
+        self
+            .environment(\.customDismissAction, customDismissAction)
+    }
+}
+
 // MARK: - Custom Dismiss Action Environment Value
 extension EnvironmentValues {
     /// Custom action that dismisses a presentation.
