86508b6256cd4e961fb8999779b2844686bb068b
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2023-07-04 00:54:39 +0400

    Add size category to UILabel height calculation methods

--- Sources/VCore/Extensions/UIKit/UILabel.LineHeights.swift
+++ Sources/VCore/Extensions/UIKit/UILabel.LineHeights.swift
@@ -47,6 +47,12 @@ extension UILabel {
             if #available(iOS 14.0, tvOS 14.0, *) {
                 label.lineBreakStrategy = lineBreakStrategy
             }
+
+            if #available(iOS 15.0, tvOS 15.0, *) {
+                label.adjustsFontForContentSizeCategory = adjustsFontForContentSizeCategory
+                label.minimumContentSizeCategory = minimumContentSizeCategory
+                label.maximumContentSizeCategory = maximumContentSizeCategory
+            }
             
             return label
         }()
@@ -116,6 +122,12 @@ extension UILabel {
             
             if #available(iOS 14.0, tvOS 14.0, *) {
                 label.lineBreakStrategy = lineBreakStrategy
+            }
+
+            if #available(iOS 15.0, tvOS 15.0, *) {
+                label.adjustsFontForContentSizeCategory = adjustsFontForContentSizeCategory
+                label.minimumContentSizeCategory = minimumContentSizeCategory
+                label.maximumContentSizeCategory = maximumContentSizeCategory
             }
             
             label.preferredMaxLayoutWidth = width ?? preferredMaxLayoutWidth
