7f811a38d7dd39fb1f3fa69295f3375609cb718b
Vakhtang Kontridze <vakho.kontridze@gmail.com>
2023-08-06 02:10:55 +0400

    Add View.getSafeAreaInsets(_:) method

---
+++ Sources/VCore/Extensions/SwiftUI/View/View.GetSafeAreaInsets.swift
@@ -0,0 +1,42 @@
+//
+//  View.GetSafeAreaInsets.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 06.08.23.
+//
+
+import SwiftUI
+
+// MARK: - View Get Safe Area Insets
+extension View {
+    /// Retrieves `EdgeInsets` from `View`.
+    ///
+    ///     @State private var safeAreaInsets: EdgeInsets = .init()
+    ///
+    ///     var body: some View {
+    ///         VStack(content: {
+    ///             Color.accentColor
+    ///                 .getSafeAreaInsets({ safeAreaInsets = $0 })
+    ///         })
+    ///     }
+    ///
+    public func getSafeAreaInsets(
+        _ action: @escaping (EdgeInsets) -> Void
+    ) -> some View {
+        self
+            .background(
+                GeometryReader(content: { proxy in
+                    Color.clear
+                        .preference(key: SafeAreaInsetsPreferenceKey.self, value: proxy.safeAreaInsets)
+                })
+            )
+            .onPreferenceChange(SafeAreaInsetsPreferenceKey.self, perform: action)
+    }
+}
+
+// MARK: - SafeAreaInsets Preference Key
+private struct SafeAreaInsetsPreferenceKey: PreferenceKey {
+    static var defaultValue: EdgeInsets = .init()
+
+    static func reduce(value: inout EdgeInsets, nextValue: () -> EdgeInsets) {}
+}
