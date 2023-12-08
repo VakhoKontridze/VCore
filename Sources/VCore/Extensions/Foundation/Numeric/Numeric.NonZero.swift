b034d122e6b6d63a271330c74b0df8aee950dca4
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2023-12-08 12:19:30 +0400

    Fix documentation

--- Sources/VCore/Extensions/Foundation/Numeric/Numeric.NonZero.swift
+++ Sources/VCore/Extensions/Foundation/Numeric/Numeric.NonZero.swift
@@ -11,8 +11,8 @@ import Foundation
 extension Numeric {
     /// Returns non-zero sized `Numeric`, or `nil`.
     ///
-    ///     let number1: Int? = 0.nonEmpty // nil
-    ///     let number2: Int? = 1.nonEmpty // 1
+    ///     let number1: Int? = 0.nonZero // nil
+    ///     let number2: Int? = 1.nonZero // 1
     ///
     public var nonZero: Self? {
         guard self != 0 else { return nil }
