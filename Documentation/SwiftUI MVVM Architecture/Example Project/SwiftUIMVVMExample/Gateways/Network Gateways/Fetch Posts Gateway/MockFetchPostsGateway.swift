dd2df7b28d808586af8c7e333af8abfa6c1e9f9b
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2024-04-19 18:34:25 +0400

    Clean up code

--- Documentation/SwiftUI MVVM Architecture/Example Project/SwiftUIMVVMExample/Gateways/Network Gateways/Fetch Posts Gateway/MockFetchPostsGateway.swift
+++ Documentation/SwiftUI MVVM Architecture/Example Project/SwiftUIMVVMExample/Gateways/Network Gateways/Fetch Posts Gateway/MockFetchPostsGateway.swift
@@ -5,6 +5,8 @@
 //  Created by Vakhtang Kontridze on 19.06.22.
 //
 
+#if DEBUG
+
 import Foundation
 
 // MARK: - Mock Fetch Posts Gateway
@@ -13,3 +15,5 @@ struct MockFetchPostsGateway: FetchPostsGatewayProtocol {
         .mock
     }
 }
+
+#endif
