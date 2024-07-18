ef484a37fdcd7728e314058752ccda13fad2caf8
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2024-07-18 22:43:20 +0400

    Fix typo

--- Sources/VCore/Models/AbsoluteFractionMeasurement.swift
+++ Sources/VCore/Models/AbsoluteFractionMeasurement.swift
@@ -29,7 +29,7 @@ public enum AbsoluteFractionMeasurement: Equatable, Hashable {
     }
 
     /// Converts absolute to fractional dimension.
-    public func toFractions(
+    public func toFraction(
         dimension: CGFloat
     ) -> CGFloat {
         switch self {
