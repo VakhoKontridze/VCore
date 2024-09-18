c7f623470473427779d365a7549c67b65de5b1ed
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2024-09-18 12:58:33 +0400

    Use pack iteration in KeyPath equality and comparison functions

---
+++ Sources/VCore/Global Functions/KeyPathEqualityAndCompraison.swift
@@ -0,0 +1,105 @@
+//
+//  KeyPathEqualityAndCompraison.swift
+//  VCore
+//
+//  Created by Vakhtang Kontridze on 18.06.22.
+//
+
+import Foundation
+
+// MARK: - Is Equal by KeyPath
+/// Returns a `Bool` value indicating whether the value of the first argument is equal to that of the second argument with given `KeyPath`s.
+///
+///     isEqual(objectA, to: objectB, by: \.a, \.b)
+///
+public func isEqual<T, each Property: Equatable>(
+    _ lhs: T,
+    to rhs: T,
+    by keyPaths: repeat KeyPath<T, each Property>
+) -> Bool {
+    for keyPath in repeat each keyPaths {
+        guard lhs[keyPath: keyPath] == rhs[keyPath: keyPath] else {
+            return false
+        }
+    }
+    
+    return true
+}
+
+// MARK: - Is Less than by KeyPath
+/// Returns a `Bool` value indicating whether the value of the first argument is less than that of the second argument with given `KeyPath`s.
+///
+///     isLess(objectA, than: objectB, by: \.a, \.b)
+///
+public func isLess<T, each Property: Comparable>(
+    _ lhs: T,
+    than rhs: T,
+    by keyPaths: repeat KeyPath<T, each Property>
+) -> Bool {
+    for keyPath in repeat each keyPaths {
+        print(lhs[keyPath: keyPath], rhs[keyPath: keyPath])
+        
+        guard lhs[keyPath: keyPath] < rhs[keyPath: keyPath] else {
+            return false
+        }
+    }
+    
+    return true
+}
+
+// MARK: - Is Less than or Equal to by KeyPath
+/// Returns a `Bool` value indicating whether the value of the first argument is less than or equal to that of the second argument with given `KeyPath`s.
+///
+///     isLessThanOrEqual(objectA, to: objectB, by: \.a, \.b)
+///
+public func isLessThanOrEqual<T, each Property: Comparable>(
+    _ lhs: T,
+    to rhs: T,
+    by keyPaths: repeat KeyPath<T, each Property>
+) -> Bool {
+    for keyPath in repeat each keyPaths {
+        guard lhs[keyPath: keyPath] <= rhs[keyPath: keyPath] else {
+            return false
+        }
+    }
+    
+    return true
+}
+
+// MARK: - Is Greater than by KeyPath
+/// Returns a `Bool` value indicating whether the value of the first argument is greater than that of the second argument with given `KeyPath`s.
+///
+///     isGreater(objectA, than: objectB, by: \.a, \.b)
+///
+public func isGreater<T, each Property: Comparable>(
+    _ lhs: T,
+    than rhs: T,
+    by keyPaths: repeat KeyPath<T, each Property>
+) -> Bool {
+    for keyPath in repeat each keyPaths {
+        guard lhs[keyPath: keyPath] > rhs[keyPath: keyPath] else {
+            return false
+        }
+    }
+    
+    return true
+}
+
+// MARK: - Is Greater than or Equal to by KeyPath
+/// Returns a `Bool` value indicating whether the value of the first argument is greater than or equal to that of the second argument with given `KeyPath`s.
+///
+///     isGreaterThanOrEqual(objectA, to: objectB, by: \.a, \.b)
+///
+public func isGreaterThanOrEqual<T, each Property: Comparable>(
+    _ lhs: T,
+    to rhs: T,
+    by keyPaths: repeat KeyPath<T, each Property>
+) -> Bool {
+    for keyPath in repeat each keyPaths {
+        guard lhs[keyPath: keyPath] >= rhs[keyPath: keyPath] else {
+            return false
+        }
+    }
+    
+    return true
+}
---
+++ Tests/VCoreTests/Tests/Global Functions/KeyPathEqualityAndCompraisonTests.swift
@@ -0,0 +1,127 @@
+//
+//  KeyPathEqualityAndCompraisonTests.swift
+//  VCoreTests
+//
+//  Created by Vakhtang Kontridze on 18.06.22.
+//
+
+import Foundation
+import XCTest
+@testable import VCore
+
+// MARK: - Tests
+final class KeyPathEqualityAndCompraisonTests: XCTestCase {
+    // MARK: Test Data
+    private struct Object {
+        var a: Int = 0
+        var b: Int = 0
+        var c: Int = 0
+    }
+    
+    // MARK: Tests
+    func testIsEqual() {
+        XCTAssertTrue(
+            VCore.isEqual(
+                Object(),
+                to: Object(),
+                by: \.a, \.b, \.c
+            )
+        )
+
+        XCTAssertFalse(
+            VCore.isEqual(
+                Object(),
+                to: Object(c: 1),
+                by: \.a, \.b, \.c
+            )
+        )
+    }
+    
+    func testIsLess() {
+        XCTAssertTrue(
+            isLess(
+                Object(),
+                than: Object(a: 1, b: 1, c: 1),
+                by: \.a, \.b, \.c
+            )
+        )
+        
+        XCTAssertFalse(
+            isLess(
+                Object(),
+                than: Object(),
+                by: \.a, \.b, \.c
+            )
+        )
+    }
+    
+    func testIsLessThanOrEqual() {
+        XCTAssertTrue(
+            isLessThanOrEqual(
+                Object(),
+                to: .init(c: 1),
+                by: \.a, \.b, \.c
+            )
+        )
+        
+        XCTAssertTrue(
+            isLessThanOrEqual(
+                Object(),
+                to: Object(),
+                by: \.a, \.b, \.c
+            )
+        )
+        
+        XCTAssertFalse(
+            isLessThanOrEqual(
+                Object(),
+                to: .init(c: -1),
+                by: \.a, \.b, \.c
+            )
+        )
+    }
+    
+    func testIsGreater() {
+        XCTAssertTrue(
+            isGreater(
+                Object(),
+                than: Object(a: -1, b: -1, c: -1),
+                by: \.a, \.b, \.c
+            )
+        )
+
+        XCTAssertFalse(
+            isGreater(
+                Object(),
+                than: Object(),
+                by: \.a, \.b, \.c
+            )
+        )
+    }
+    
+    func testIsGreaterThanOrEqual() {
+        XCTAssertTrue(
+            isGreaterThanOrEqual(
+                Object(),
+                to: Object(c: -1),
+                by: \.a, \.b, \.c
+            )
+        )
+        
+        XCTAssertTrue(
+            isGreaterThanOrEqual(
+                Object(),
+                to: Object(),
+                by: \.a, \.b, \.c
+            )
+        )
+        
+        XCTAssertFalse(
+            isGreaterThanOrEqual(
+                Object(),
+                to: Object(c: 1),
+                by: \.a, \.b, \.c
+            )
+        )
+    }
+}
