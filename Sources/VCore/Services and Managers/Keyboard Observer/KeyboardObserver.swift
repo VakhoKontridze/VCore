d5200c8071d16f8dcb0ee67d89e4dd38de18f2c0
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2024-10-21 23:34:51 +0400

    Fix concurrency issue

--- Sources/VCore/Services and Managers/Keyboard Observer/KeyboardObserver.swift
+++ Sources/VCore/Services and Managers/Keyboard Observer/KeyboardObserver.swift
@@ -88,6 +88,7 @@ public final class KeyboardObserver: ObservableObject { // TODO: iOS 17.0 - Conv
     // MARK: Keyboard Management
 #if canImport(UIKit) && !os(watchOS)
     
+    @MainActor // Needed, despite `MainActor` declared on class-level
     private func keyboardWillShow(notification: Notification) {
         let systemKeyboardInfo: SystemKeyboardInfo = .init(notification: notification)
         
@@ -136,6 +137,7 @@ public final class KeyboardObserver: ObservableObject { // TODO: iOS 17.0 - Conv
         }
     }
 
+    @MainActor // Needed, despite `MainActor` declared on class-level
     private func keyboardWillHide(notification: Notification) {
         let systemKeyboardInfo: SystemKeyboardInfo = .init(notification: notification)
 
