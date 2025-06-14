596b2599ca2cbc1a87fcd2e40c0bcb02089abc09
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2025-06-14 10:46:32 +0400

    Aggregate gateway files into one

--- Extra/XCode Templates/Gateway.xctemplate/___FILEBASENAME___Gateway.swift
+++ Extra/XCode Templates/Gateway.xctemplate/___FILEBASENAME___Gateway.swift
@@ -3,9 +3,43 @@
 import Foundation
 import VCore
 
+// MARK: - ___VARIABLE_productName___ Gateway Protocol
+protocol ___VARIABLE_productName___GatewayProtocol {
+    func fetch(with input: ___VARIABLE_productName___GatewayInput) async throws -> ___VARIABLE_productName___GatewayOutput
+}
+
+// MARK: - ___VARIABLE_productName___ Gateway Input
+@CodingKeysGeneration
+struct ___VARIABLE_productName___GatewayInput: Encodable {
+    // ...
+}
+
+// MARK: - ___VARIABLE_productName___ Gateway Output
+@CodingKeysGeneration
+struct ___VARIABLE_productName___GatewayOutput: Decodable {
+    // MARK: Properties
+    // ...
+
+    // MARK: Mock
+#if DEBUG
+    static var mock: Self {
+        .init()
+    }
+#endif
+}
+
 // MARK: - ___VARIABLE_productName___ Gateway
 struct ___VARIABLE_productName___Gateway: ___VARIABLE_productName___GatewayProtocol {
     func fetch(with input: ___VARIABLE_productName___GatewayInput) async throws -> ___VARIABLE_productName___GatewayOutput {
         FIXME()
     }
 }
+
+// MARK: - Mock ___VARIABLE_productName___ Gateway
+struct Mock___VARIABLE_productName___Gateway: ___VARIABLE_productName___GatewayProtocol {
+    func fetch(with input: ___VARIABLE_productName___GatewayInput) async throws -> ___VARIABLE_productName___GatewayOutput {
+        .mock
+    }
+}
+
+#endif
--- Extra/XCode Templates/Gateway.xctemplate/___FILEBASENAME___GatewayProtocol.swift
+++
@@ -1,8 +0,0 @@
-//  ___FILEHEADER___
-
-import Foundation
-
-// MARK: - ___VARIABLE_productName___ Gateway Protocol
-protocol ___VARIABLE_productName___GatewayProtocol {
-    func fetch(with input: ___VARIABLE_productName___GatewayInput) async throws -> ___VARIABLE_productName___GatewayOutput
-}
