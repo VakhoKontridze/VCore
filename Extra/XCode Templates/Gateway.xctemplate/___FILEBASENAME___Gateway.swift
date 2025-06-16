d7eed6e4fc902cd654646274776b95ae9c38082a
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2025-06-16 11:41:07 +0400

    Fix error in template

--- Extra/XCode Templates/Gateway.xctemplate/___FILEBASENAME___Gateway.swift
+++ Extra/XCode Templates/Gateway.xctemplate/___FILEBASENAME___Gateway.swift
@@ -35,6 +35,8 @@ struct ___VARIABLE_productName___Gateway: ___VARIABLE_productName___GatewayProto
     }
 }
 
+#if DEBUG
+
 // MARK: - Mock ___VARIABLE_productName___ Gateway
 struct Mock___VARIABLE_productName___Gateway: ___VARIABLE_productName___GatewayProtocol {
     func fetch(with input: ___VARIABLE_productName___GatewayInput) async throws -> ___VARIABLE_productName___GatewayOutput {
