13bb6a29301c8f3e0352385bc1d9516c64e0486a
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2026-04-23 13:03:57 +0400

    Deprecate Collection.enumeratedArray

--- Sources/VCore/Core/Extensions/Foundation/Collection/Collection+EnumeratedArray.swift
+++ Sources/VCore/Core/Extensions/Foundation/Collection/Collection+EnumeratedArray.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-nonisolated extension Collection {
+nonisolated extension Collection { // TODO: iOS 26.0 - Remove
     /// Enumerated a `Collection` as a tuple of offset and element.
     ///
     /// Element can be used as id in `ForEach` without use of an explicit identifier.
@@ -18,6 +18,11 @@ nonisolated extension Collection {
     ///         }
     ///     }
     ///
+    @available(iOS, deprecated: 26.0, message: "Use native 'enumerated(...)' method instead")
+    @available(macOS, deprecated: 26.0, message: "Use native 'enumerated(...)' method instead")
+    @available(tvOS, deprecated: 26.0, message: "Use native 'enumerated(...)' method instead")
+    @available(watchOS, deprecated: 26.0, message: "Use native 'enumerated(...)' method instead")
+    @available(visionOS, deprecated, message: "Use native 'enumerated(...)' method instead")
     public func enumeratedArray() -> [(offset: Int, element: Element)] {
         .init(enumerated())
     }
