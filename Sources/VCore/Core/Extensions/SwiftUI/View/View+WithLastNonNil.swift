95bda4d6b9744fd9ba27c1dab0f7b569ceeb4898
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2026-05-30 14:07:47 +0400

    Add secondary View.withLastNonNil(_:content) method for Equatable types

--- Sources/VCore/Core/Extensions/SwiftUI/View/View+WithLastNonNil.swift
+++ Sources/VCore/Core/Extensions/SwiftUI/View/View+WithLastNonNil.swift
@@ -6,7 +6,6 @@
 //
 
 public import SwiftUI
-import Combine
 
 extension View {
     /// Caches and uses last non-`nil` item when composing content.
@@ -54,6 +53,78 @@ extension View {
             Content: View
     {
         LastNonNilCachingView(
+            key: item?.id,
+            item: item,
+            root: self,
+            content: content
+        )
+    }
+    
+    /// Caches and uses last non-`nil` item when composing content.
+    ///
+    /// Used for `ModalPresenter` API.
+    ///
+    ///     extension View {
+    ///         func modal<Item, Content>(
+    ///             link: ModalPresenterLink,
+    ///             item: Binding<Item?>,
+    ///             @ViewBuilder content: @escaping (Item) -> Content
+    ///         ) -> some View
+    ///             where
+    ///                 Item: Identifiable,
+    ///                 Content: View
+    ///         {
+    ///             let isPresented: Binding<Bool> = .init(
+    ///                 get: { item.wrappedValue != nil },
+    ///                 set: { if !$0 { item.wrappedValue = nil } }
+    ///             )
+    ///
+    ///             return self
+    ///                 .withLastNonNil(item.wrappedValue) { (view, item) in
+    ///                     view
+    ///                         .modalPresenterLink(
+    ///                             link: link,
+    ///                             isPresented: isPresented,
+    ///                         ) {
+    ///                             Modal<Content?>(isPresented: isPresented) {
+    ///                                 if let item {
+    ///                                     content(item)
+    ///                                 }
+    ///                             }
+    ///                         }
+    ///                 }
+    ///         }
+    ///     }
+    ///
+    public func withLastNonNil<Item, Content>(
+        _ item: Item?,
+        @ViewBuilder content: @escaping (Self, Item?) -> Content
+    ) -> some View
+        where
+            Item: Equatable,
+            Content: View
+    {
+        LastNonNilCachingView(
+            key: item,
+            item: item,
+            root: self,
+            content: content
+        )
+    }
+    
+    /// Caches and uses last non-`nil` item when composing content.
+    ///
+    /// For additional info, refer to `View.withLastNonNil(_:content)` method.
+    public func withLastNonNil<Item, Content>(
+        _ item: Item?,
+        @ViewBuilder content: @escaping (Self, Item?) -> Content
+    ) -> some View
+        where
+            Item: Identifiable & Equatable,
+            Content: View
+    {
+        LastNonNilCachingView(
+            key: item,
             item: item,
             root: self,
             content: content
@@ -61,28 +132,34 @@ extension View {
     }
 }
 
-private struct LastNonNilCachingView<Item, Root, Content>: View
+private struct LastNonNilCachingView<Item, Key, Root, Content>: View
     where
-        Item: Identifiable,
+        Key: Equatable,
         Root: View,
         Content: View
 {
     // MARK: Properties
+    private let key: Key
     private let item: Item?
+    
     @State private var lastNonNilItem: Item?
     
     private let root: Root
     
     private let content: (Root, Item?) -> Content
-    
+
     // MARK: Initializers
     init(
+        key: Key,
         item: Item?,
         root: Root,
         @ViewBuilder content: @escaping (Root, Item?) -> Content
     ) {
+        self.key = key
+        
         self.item = item
         self._lastNonNilItem = State(initialValue: item)
+        
         self.root = root
         self.content = content
     }
@@ -90,7 +167,7 @@ private struct LastNonNilCachingView<Item, Root, Content>: View
     // MARK: Body
     var body: some View {
         content(root, item ?? lastNonNilItem)
-            .onChange(of: item?.id) {
+            .onChange(of: key) {
                 if let item {
                     lastNonNilItem = item
                 }
