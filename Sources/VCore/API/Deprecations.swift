870f07589616a812eba45a15da6ec3ff8e170612
Vakhtang Kontridze <vakhtang.kontridze@gmail.com>
2026-07-06 10:23:42 +0400

    Deprecate URL init with path and query parameters

--- Sources/VCore/API/Deprecations.swift
+++ Sources/VCore/API/Deprecations.swift
@@ -1435,3 +1435,44 @@ nonisolated extension CachedKeyedManagedTask {
         )
     }
 }
+
+nonisolated extension URL {
+    @available(*, deprecated, message: "Will be removed in '9.0.0'")
+    public init?(
+        string: String,
+        pathParameters: [String],
+        queryParameters: [(key: String, value: String)]
+    ) {
+        var string = string
+        if string.hasSuffix("/") { _ = string.removeLast() }
+
+        guard !string.isEmpty else { return nil }
+
+        pathParameters.forEach { string.append("/\($0)") }
+
+        guard var urlComponents: URLComponents = .init(string: string) else { return nil }
+        urlComponents.addQueryItems(queryParameters)
+
+        guard let url: URL = urlComponents.url else { return nil }
+
+        self = url
+    }
+}
+
+nonisolated extension URLComponents {
+    fileprivate mutating func addQueryItems(
+        _ newQueryItems: [(key: String, value: String)]
+    ) {
+        guard !newQueryItems.isEmpty else { return }
+
+        let newURLQueryItems: [URLQueryItem] = newQueryItems.map { (key, value) in
+            URLQueryItem(name: key, value: value)
+        }
+
+        if queryItems == nil {
+            queryItems = newURLQueryItems
+        } else {
+            queryItems?.append(contentsOf: newURLQueryItems)
+        }
+    }
+}
